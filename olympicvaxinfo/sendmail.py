a45b73856c1d91a64a5fd9d6aad8208b18682893
Cameron <cameronlambert98@gmail.com>
2021-02-14 16:35:47 -0800

    fixed typo

--- olympicvaxinfo/sendmail.py
+++ olympicvaxinfo/sendmail.py
@@ -1,6 +1,6 @@
 import os
 
 def sendmail(subject, body):
-    command = 'echo "' + '<a href="[[UNSUB_LINK_LOCALE]]">click here to unsubscribe</a>' + '\n' + body + " | mailx -r jeffcovax@cameronlambert.com -s "' + subject + '" xntl5lpu1@lists.mailjet.com'
+    command = 'echo "' + '<a href="[[UNSUB_LINK_LOCALE]]">click here to unsubscribe</a>' + '\n' + body + '" | mailx -r jeffcovax@cameronlambert.com -s "' + subject + '" xntl5lpu1@lists.mailjet.com'
     print("sending mail to mailing list")
     os.system(command)

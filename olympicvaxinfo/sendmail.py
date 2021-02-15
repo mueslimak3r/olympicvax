5e05b88f976ad1f6ab60ae4adf6117c697d25aa3
Cameron <cameronlambert98@gmail.com>
2021-02-14 16:21:34 -0800

    added unsubscribe link to email

--- olympicvaxinfo/sendmail.py
+++ olympicvaxinfo/sendmail.py
@@ -1,7 +1,6 @@
 import os
 
-
 def sendmail(subject, body):
-    command = 'echo "' + body + '" | mailx -r jeffcovax@cameronlambert.com -s "' + subject + '" xntl5lpu1@lists.mailjet.com'
+    command = 'echo "' + body + '\n' + '<a href="[[UNSUB_LINK_LOCALE]]">click here to unsubscribe</a>' + '" | mailx -r jeffcovax@cameronlambert.com -s "' + subject + '" xntl5lpu1@lists.mailjet.com'
     print("sending mail to mailing list")
     os.system(command)
